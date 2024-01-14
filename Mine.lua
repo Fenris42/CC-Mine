1fb6350c7d680fa318009c6e2d2880b28b805915
Matt Todd <mtodd447@gmail.com>
2024-01-13 19:03:53 -0800

    Version 2
    Reworked script entirely
    - created move functions for less redundant code
    - created search inventory function
    - changed tunnel pattern
    - turtle will now mine ore on the edges of the tunnel
    - added "poop" function where turtle will take a bathroom break to unload cobble when inventory full
    -

--- Mine.lua
+++ Mine.lua
@@ -1,354 +1,570 @@
-local distanceTraveled = 0
+--Movement--
 
----
+function forward(x)
 
-function Tunnel()
---dig a 3x3 tunnel 2 blocks long
-	
-	print("Tunneling...")
-	print("")
-	
-	--left column
-	Dig()
-	Bridge()
-	
-	turtle.up()
-	Dig()
-	
-	turtle.up()
-	Dig()
-	
-	--middle column
-	turtle.turnRight()
-	turtle.forward()
-	turtle.turnLeft()
-	Dig()
-	
-	turtle.down()
-	Dig()
-	
-	turtle.down()
-	Dig()
-	Bridge()
-	
-	--right column
-	turtle.turnRight()
-	turtle.forward()
-	turtle.turnLeft()
-	Dig()
-	Bridge()
-	
-	turtle.up()
-	Dig()
-	
-	turtle.up()
-	Dig()
-	
-	--move forward
-	turtle.forward()
-	distanceTraveled = distanceTraveled + 1
-	
-	--right column
-	Dig()
-	
-	turtle.down()
-	Dig()
-	
-	turtle.down()
-	Dig()
-	
-	Bridge()
-	
-	--middle column
-	turtle.turnLeft()
-	turtle.forward()
-	turtle.turnRight()
-	Dig()
-	Bridge()
-	
-	turtle.up()
-	Dig()
-	
-	turtle.up()
-	Dig()
-	
-	--left column
-	turtle.turnLeft()
-	turtle.forward()
-	turtle.turnRight()
-	Dig()
-	
-	turtle.down()
-	Dig()
-	
-	turtle.down()
-	Dig()
-	Bridge()
-	
-	--move forward
-	turtle.forward()
-	distanceTraveled = distanceTraveled + 1
-
-end
-
----
-
-function Dig()
---dig repeatedly until block is clear
-
-	local blockDetected = turtle.inspect()
-	
-	while(blockDetected == true) do
-	
-		turtle.dig()
-		blockDetected = turtle.inspect()
-		
-	end
-end
-
----
-
-function Bridge()
---build bridge under turtle if floor not solid
-
-	local success, block = turtle.inspectDown()
-	local bridgeRequired = false
-	
-	if(success == true) then
-	--there is a block below
-	
-		if(block.name == "minecraft:water" or block.name == "minecraft:lava") then
-		--block below is liquid
-			bridgeRequired = true
-		end
-		
-	else
-	--block below is air
-		bridgeRequired = true
-	end
-	
-	if(bridgeRequired == true) then
-	--search inventory for cobble
-		
-		for i = 2,16 do
-			turtle.select(i)
-			
-			local slot = turtle.getItemDetail()
-			
-			if(slot.name == "minecraft:cobblestone" or slot.name == "minecraft:cobbled_deepslate") then
-				--place bridge
-				turtle.placeDown()
-				
-				print("Placed Bridge")
-				print("")
-				
-				break
-				
-			end
-		end
-	end
-end
-
---- 
-
-
----
-
-function Refuel()
---refuel to required travel distance
-	
-	print("Checking Fuel")
-	
-	local fuelRequired = 0
-	local minFuel = 25
-	
-	--select fuel slot
-	turtle.select(1)
-	
-	--determine fuel level required
-	if(distanceTraveled > minFuel) then
-		fuelRequired = distanceTraveled
-	else
-		--min required fuel for next tunnel call
-		fuelRequired = minFuel
-	end
-	
-	print("Current Fuel: ", turtle.getFuelLevel())
-	print("Required Fuel: ", fuelRequired)
-	print("")
-	
-	--refuel
-	while(turtle.getFuelLevel() < fuelRequired) do
-	
-		local slot = turtle.getItemDetail()
-		
-		if(slot.name == "minecraft:coal" or slot.name == "minecraft:charcoal") then
-			
-			turtle.refuel(1)
-			print("Added Fuel")
-			print("")
-			
-		else
-		--fuel slot is empty or invalid
-			print("No Fuel")
-			return
-		end
-	end
-end
-
----
-
-function FuelAvailable()
---ensure available fuel is sufficient to be able to return home
-	
-	local fuelAvailable = 0
-	
-	--select fuel slot
-	turtle.select(1)
-	
-	local slot = turtle.getItemDetail()
-		
-	if(slot.name == "minecraft:coal" or slot.name == "minecraft:charcoal") then
-			
-		fuelAvailable = slot.count * 80
-		
-	end
-	
-	if(fuelAvailable > distanceTraveled) then
-	
-		return true
-		
-	else
-	
-		print("Insufficient Fuel")
-		print("")
-		
-		return false
-		
-	end
-end
-
----
-
-function InventoryFull()
-	
-	local isFull = true;
-	
-	for i = 2,16 do
-		turtle.select(i)
-			
-		if(turtle.getItemDetail() == nil) then
-			isFull = false;
-			break
-		end
-	end
-	
-	if(isFull == true) then
-	
-		print("Inventory Full")
-		print("")
-		
-	end
-	
-	return isFull
-	
-end
-
----
-
-function Torch()
-	
-	if(distanceTraveled % 10 == 0) then
-		
-		print("Lighting Torch")
-		print("")
-		
-		--select torch slot
-		turtle.select(2)
-	
-		local slot = turtle.getItemDetail()
-		
-		if(slot.name == "minecraft:torch") then
-			
-			--reposition to inspect wall
-			turtle.turnLeft()
-			turtle.turnLeft()
-			turtle.forward()
-			turtle.turnRight()
-			turtle.up()
-			
-			if(turtle.inspect() == false) then
-			--block is an air tile
-			
-				--search inventory for cobble to patch wall
-				for i = 2,16 do
-					turtle.select(i)
-			
-					local slot = turtle.getItemDetail()
-			
-					if(slot.name == "minecraft:cobblestone" or slot.name == "minecraft:cobbled_deepslate") then
-						
-						--place patch
-						turtle.place()
-				
-						print("Patched Wall")
-						print("")
-						
-						--reselect torch slot
-						turtle.select(2)
-						
-						break
-				
-					end
-				end
-			end
-			
-			--place torch
-			turtle.down()
-			turtle.placeUp()
-			
-			--reposition back to 0
-			turtle.turnRight()
-			turtle.forward()
-			
-		else
-			print("Out of Torches")
-			print("")
-		end	
-	end
-end
-
----
-
-function RetunToStart()
-	
-	print("Returning Home")
-	
-	turtle.turnRight()
-	turtle.forward()
-	turtle.turnRight()
-	
-	for i = 0,distanceTraveled do
+	for i = 1,x do
 		turtle.forward()
 	end
 	
+end
+
+---
+
+function back(x)
+
+	for i = 1,x do
+		turtle.back()
+	end
 	
 end
 
---Main--
+---
 
-Refuel()
+function left(x)
 
---reposition
-turtle.turnLeft()
-turtle.forward()
-turtle.turnRight()
-
---tunnel
-while(InventoryFull() == false and FuelAvailable() == true) do
-	Refuel()
-	Tunnel()
-	Torch()
+	for i = 1,x do
+		turtle.turnLeft()
+	end
+	
 end
 
-RetunToStart()
+---
+
+function right(x)
+
+	for i = 1,x do
+		turtle.turnRight()
+	end
+	
+end
+
+---
+
+function up(x)
+
+	for i = 1,x do
+		turtle.up()
+	end
+	
+end
+
+---
+
+function down(x)
+
+	for i = 1,x do
+		turtle.down()
+	end
+	
+end
+
+---
+
+function retunToStart()
+	
+	print("Returning home")
+	print("")
+	
+	right(2)
+	forward(distanceTraveled)
+	
+	print("Job done")
+	
+end
+
+
+
+--Mining--
+
+function tunnel()
+-- dig a 3x3 tunnel and mine in a 5x5 area
+
+	print("Tunneling...")
+	print("")
+	
+	--tunnel bottom center
+	dig()
+	forward(1)
+	
+	--floor center
+	oreDown()
+	patchDown()
+	
+	--tunnel bottom left
+	left(1)
+	dig()
+	
+	--floor left
+	forward(1)
+	oreDown()
+	patchDown()
+	
+	--left wall bottom
+	ore()
+	
+	--tunnel center left
+	digUp()
+	up(1)
+	
+	--left wall center
+	ore()
+	
+	--tunnel top left
+	digUp()
+	up(1)
+	
+	--left wall top
+	ore()
+	
+	--ceiling left
+	oreUp()
+	
+	--tunnel top center
+	right(2)
+	dig()
+	
+	--ceiling center
+	forward(1)
+	oreUp()
+	
+	--tunnel top right
+	dig()
+	
+	--ceiling right
+	forward(1)
+	oreUp()
+	
+	--right wall top
+	ore()
+	
+	--tunnel center right
+	digDown()
+	
+	--right wall center
+	down(1)
+	ore()
+	
+	--tunnel bottom right
+	digDown()
+	
+	--right wall bottom
+	down(1)
+	ore()
+	
+	--floor right
+	oreDown()
+	patchDown()
+	
+	--recenter
+	left(2)
+	forward(1)
+	right(1)
+	
+	--tunnel center (leaving to last prevents center area from piling up with sand/gravel)
+	digUp()
+	
+	--floor center (retry in case no cobble to start)
+	patchDown()
+
+end
+
+---
+
+function dig()
+--dig repeatedly forward until block is clear
+	
+	while(turtle.detect() == true) do
+		turtle.dig()
+	end
+	
+end
+
+---
+
+function digUp()
+--dig repeatedly up until block is clear
+
+	while(turtle.detectUp() == true) do
+		turtle.digUp()
+	end
+	
+end
+
+---
+
+function digDown()
+	
+	turtle.digDown()
+	
+end
+
+---
+
+function ore()
+--inspect wall for ore and mine if found
+
+	local success, block = turtle.inspect()
+	
+	if(success == true) then
+		
+		if(string.find(block.name, "ore")) then
+		
+			dig()
+			
+		end
+		
+	end
+	
+end
+
+---
+
+function oreUp()
+--inspect ceiling for ore and mine if found
+
+	local success, block = turtle.inspectUp()
+	
+	if(success == true) then
+		
+		if(string.find(block.name, "ore")) then
+		
+			digUp()
+			
+		end
+		
+	end
+	
+end
+
+---
+
+function oreDown()
+--inspect floor for ore and mine if found
+
+	local success, block = turtle.inspectDown()
+	
+	if(success == true) then
+		
+		if(string.find(block.name, "ore")) then
+		
+			digDown()
+			
+		end
+		
+	end
+	
+end
+
+
+
+--Patching--
+
+function patch()
+--patch wall with cobble
+
+	local block = turtle.detect()
+	
+	if(block == false) then
+	--no block detected, patch
+	
+		if(search("cobble") == true) then
+		
+			turtle.place()
+			print("Patched wall")
+			print("")
+			
+		else
+		
+			print("Out of cobble")
+			print("")
+			
+		end
+		
+	end
+	
+end
+
+---
+
+function patchDown()
+--patch floor with cobble
+
+	local block = turtle.detectDown()
+	
+	if(block == false) then
+	--no block detected, patch
+	
+		if(search("cobble") == true) then
+		
+			turtle.placeDown()
+			print("Floor patched")
+			print("")
+			
+		else
+		
+			print("Out of cobble")
+			print("")
+			
+		end
+		
+	end
+	
+end
+
+
+
+--Torch--
+
+function torch()
+--place torch every 10 blocks
+
+	if(distanceTraveled % 10 == 0) then
+	
+		--check if torches available
+		if(search("torch") == true) then
+			
+			print("Lighting torch")
+			print("")
+			
+			--reposition
+			left(1)
+			forward(1)
+			up(1)
+			
+			--patch wall if necessary
+			patch()
+			
+			--place torch
+			back(1)
+			search("torch")
+			turtle.place()
+			
+			--recenter
+			down(1)
+			right(1)
+
+		else
+		
+			print("Out of torches")
+			print("")
+			
+		end
+		
+	end
+	
+end
+
+
+
+--Inventory--
+
+function search(item)
+--search for and select item in inventory
+
+	local found = false
+	
+	for i = 1,16 do
+		
+		turtle.select(i)
+		local slot = turtle.getItemDetail()
+		
+		if(slot == nil) then
+		
+			if(item == "empty") then
+			
+				found = true
+				break
+				
+			end
+		
+		else
+			if(item == "fuel") then
+			
+				if(slot.name == "minecraft:coal" or slot.name == "minecraft:charcoal") then
+			
+					found = true
+					break
+				
+				end
+			
+			elseif(item == "torch") then
+		
+				if(slot.name == "minecraft:torch") then
+			
+					found = true
+					break
+				
+				end
+			
+			elseif(item == "cobble") then
+		
+				if(slot.name == "minecraft:cobblestone" or slot.name == "minecraft:cobbled_deepslate") then
+			
+					found = true
+					break
+				
+				end
+				
+			end
+			
+		end
+
+	end
+	
+	return found
+	
+end
+
+---
+
+function checkInventory()
+--check if inventory full
+
+	if(search("empty") == false) then
+	--no empty slot found
+		
+		if(search("cobble") == true) then
+		--search inventory for cobble to dump
+				
+			poop()
+				
+		else
+		--no dumpable inventory
+			
+			print("Inventory full")
+			print("")
+			
+			inventoryFull = true
+				
+		end
+		
+	end
+
+end
+
+---
+
+function poop()
+--dump cobble from inventory, reserving 1 stack for patching
+
+	print("Got to go poop")
+	print("")
+	
+	--dig poop hole
+	left(1)
+	forward(1)
+	dig()
+	forward(1)
+	digDown()
+	right(2)
+	
+	--poop cobble
+	while(search("cobble") == true) do
+		turtle.dropDown()
+	end
+	
+	--collect 1 cobble to patch hole
+	search("empty")
+	turtle.suckDown(1)
+	
+	--patch poop hole
+	forward(1)
+	right(2)
+	patch()
+	
+	--recenter
+	left(2)
+	forward(1)
+	left(1)
+	
+end
+
+
+
+--Fuel--
+
+function refuel()
+	
+	--calculate fuel levels
+	local currentFuel = turtle.getFuelLevel()
+	local minFuel = 17 + distanceTraveled --min required to complete tunnel, torch, poop and return home
+	local maxFuel = 0
+	
+	if(search("fuel") == true) then
+	--calculate how much fuel is in inventory
+	
+		maxFuel = turtle.getItemCount() * 80
+		
+	else
+	--should only happen if no fuel was supplied to turtle at start
+	
+		maxFuel = turtle.getFuelLevel()
+		
+	end
+	
+	--status report
+	print("Fuel Status:")
+	print("Fuel    : ", currentFuel)
+	print("Min Fuel: ", minFuel)
+	print("Max Fuel: ", maxFuel)
+	print("")
+	
+	--refuel
+	while(currentFuel < minFuel) do
+		
+		print("Refueling")
+		print("")
+		
+		if(search("fuel") == true) then
+			
+			turtle.refuel(1)
+			
+		else
+			
+			print("Out of fuel")
+			print("")
+			
+			outOfFuel = true
+			break
+			
+		end
+		
+		--update fuel level
+		currentFuel = turtle.getFuelLevel()
+		
+	end
+	
+	--check if sufficient fuel left
+	if(maxFuel < minFuel) then
+		
+		outOfFuel = true
+		
+		print("Out of fuel")
+		print("")
+			
+	end
+
+end
+
+
+
+--Main---------------------------------------------------------------------
+distanceTraveled = 0
+inventoryFull = false
+outOfFuel = false
+
+--tunnel
+while(inventoryFull == false and outOfFuel == false) do
+
+	refuel()
+	
+	tunnel()
+	distanceTraveled = distanceTraveled + 1
+	
+	torch()
+	
+	checkInventory()
+	
+end
+
+retunToStart()
